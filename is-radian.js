f63fc680873334b5c95238cf213d9ee12e3830eb
Frankie A <40648091+OkiStuff@users.noreply.github.com>
2024-05-16 00:15:22 +0000

    fix bug

--- is-radian.js
+++ is-radian.js
@@ -2,7 +2,7 @@ const isRadian = (x) => {
     if (typeof x === "number") return true;
     else if (typeof x === "bigint") return true;
     else if (Array.isArray(x)) return x.map((element, index, arr) => isRadian(element));
-    return undefined;
+    return false;
 }
 
 module.exports = isRadian;
